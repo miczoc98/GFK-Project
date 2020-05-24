85ffb79e76d44b8e223b9a95b77827071561057c
Wiktor Żychowicz <wiktor.zycho99@gmail.com>
2020-05-24 17:08:30 +0200

    Second version

--- GUI/GUIMyFrame1.cpp
+++ GUI/GUIMyFrame1.cpp
@@ -17,6 +17,65 @@ void GUIMyFrame1::m_button_biegun_click( wxCommandEvent& event )
 	m_uklad_kart = false;
 }
 
+void m_slider_a_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_b_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_c_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_phi_scroll(wxScrollEvent& event) 
+{
+
+}
+
+void m_slider_psi_scroll(wxScrollEvent& event)
+{
+
+}
+void m_slider_teta_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_n_scroll(wxScrollEvent& event) 
+{
+
+}
+
+void m_slider_m_scroll(wxScrollEvent& event) 
+{
+
+}
+
+void m_slider_k_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_x_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_y_scroll(wxScrollEvent& event)
+{
+
+}
+
+void m_slider_z_scroll(wxScrollEvent& event)
+{
+
+}
+
 void GUIMyFrame1::m_checkBoxDot_check( wxCommandEvent& event )
 {
 	m_checkBoxLine->SetValue(false);
